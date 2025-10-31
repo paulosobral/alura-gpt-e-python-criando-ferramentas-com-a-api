ed73d8be73d8b10e59d2a67ad2b0ebc8526ab3e7
Paulo Sobral <paulo.sobral@outlook.com.br>
2025-10-30 22:10:23 -0300

    feat: reactor ctegorizador.py

---
+++ categorizador.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+prompt_sistema = f"""
+        Você é um categorizador de produtos.
+        Você deve assumir as categorias presentes na lista abaixo.
+
+        # Lista de Categorias Válidas
+            - Moda Sustentável
+            - Produtos para o Lar
+            - Beleza Natural
+            - Eletrônicos Verdes
+            - Higiene Pessoal
+
+        # Formato da Saída
+        Produto: Nome do Produto
+        Categoria: apresente a categoria do produto
+
+        # Exemplo de Saída
+        Produto: Escova elétrica com recarga solar
+        Categoria: Eletrônicos Verdes
+    """
+
+prompt_usuario = input("Apresente o nome de um produto: ")
+
+resposta = cliente.chat.completions.create(
+    messages=[
+        {
+            "role": "system", 
+            "content": prompt_sistema
+        },
+        {
+            "role": "user", 
+            "content": prompt_usuario
+        }
+    ],
+    model = modelo,
+    temperature = 0,
+    max_tokens = 200,
+    # n = 3,
+)
+
+#  for contador in range(0,3):
+print(resposta.choices[0].message.content)
